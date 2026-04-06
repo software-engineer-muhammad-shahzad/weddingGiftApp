a5474430ffaf5229b7e3a10a0d6be5e68264de5a
mshahzad <shahzaddeveloper704@gmail.com>
2026-04-06 23:18:27 +0500

    add logo-shagun

--- app/components/auth/signup/SignupLeft.tsx
+++ app/components/auth/signup/SignupLeft.tsx
@@ -3,7 +3,11 @@
 const SignupLeft = () => {
   return (
     <div className="h-full flex flex-col justify-center">
-      <p>hello</p>
+      <div>
+
+        {/* logo */}
+        
+      </div>
     </div>
   )
 }
